c8c8da8edb2a767d4e4a300f22174eb801061e27
Josh Warwick <josh.warwick15@gmail.com>
2024-06-24 22:08:54 +0100

    prood

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.dev')
+    if os.environ.get('ENVIRONMENT') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

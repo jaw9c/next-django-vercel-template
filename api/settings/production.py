6a59eeb7ee7a485b71b5ea6509539313708e7229
Josh Warwick <josh.warwick15@gmail.com>
2024-06-24 22:02:11 +0100

    try

--- api/settings/production.py
+++ api/settings/production.py
@@ -1,5 +1,14 @@
 from .base import *
 import os
+import dj_database_url
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
+DATABASE_URL= os.environ.get('DATABASE_URL')
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
+}

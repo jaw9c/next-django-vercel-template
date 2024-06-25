acc332eafea60cca15e4cadd2c5a1cd0504e84eb
Josh Warwick <josh.warwick15@gmail.com>
2024-06-25 12:06:36 +0100

    try

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
+    path('api/admin', admin.site.urls),
     path('', include('example.urls')),
 ]

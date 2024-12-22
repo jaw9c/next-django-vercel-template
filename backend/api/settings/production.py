6cf5188656bda5752fd174a2d85e6ca99ecbadd2
Josh Warwick <josh.warwick15@gmail.com>
2024-12-22 17:25:49 +0000

    do

--- backend/api/settings/production.py
+++ backend/api/settings/production.py
@@ -4,12 +4,18 @@ import dj_database_url
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
-POSTGRES_URL= os.environ.get('POSTGRES_URL')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_DATABASE = os.environ.get('POSTGRES_DATABASE')
 
-if POSTGRES_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-        ),
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DATABASE,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": 5432,
     }
+}

7f632e5e85d485add67e1656e0013a141fbd71a6
Josh Warwick <josh.warwick15@gmail.com>
2024-12-22 17:02:05 +0000

    default

--- backend/api/settings/production.py
+++ backend/api/settings/production.py
@@ -4,9 +4,9 @@ import dj_database_url
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DATABASE_URL= os.environ.get('DATABASE_URL')
+POSTGRES_URL= os.environ.get('POSTGRES_URL')
 
-if DATABASE_URL:
+if POSTGRES_URL:
     DATABASES = {
         'default': dj_database_url.config(
             conn_max_age=600,
